3ece9fe1d8fd5cd668215624f1b4b21f0cdf1dd9
jingweizuo <jingwei.zuo.uvsq@gmail.com>
2019-05-03 00:36:51 +0200

    Modified the function "shapelet_matching in evaluation block"

--- venv/ISMAP/evaluation_block.py
+++ venv/ISMAP/evaluation_block.py
@@ -12,6 +12,7 @@ from pyspark.sql.functions import udf, split, hash, col, collect_list, collect_s
 # Global variable for the slope of Sigmoid Function
 theta = 1
 n = 0
+loss = 0
 
 master_id = "local"
 #master_id = "spark://spark-master:7077"
@@ -28,23 +29,29 @@ def sigmoid(x, theta, x0):
 
 def shapelet_matching(shap_set, TS_window):
     # using Sigmoid for Loss Function
-    # parallel computing
-    global theta, n
-    loss = 0
+    # parallel computing for TSs in the window
+    global theta, n, loss
     loss_batch = 0
+    w = len(TS_window)
     # Loss Computing, with a fading strategy or not?
-
     for ts in TS_window:
+        shap_set = {s for s in shap_set if s.Class == ts.class_timeseries}
+        min_dist = np.inf
+        min_s = shap()
+        # find the closest Shapelet to 'ts'
+        for s in shap_set:
             dist = np.min(sm.calculate_distances(ts, s.subseq, "mass_V2"))
-            h = sigmoid(dist, theta, s.thresh)
-            if ts.class_timeseries == s.Class:
-                y = 1
-            else:
-                y = 0
-            loss_batch += np.abs(h + y -1)
-            n += 1
-        loss += loss_batch
-        loss /= n
-        loss_batch /= len(TS_window)
-
+            if min_dist > dist:
+                min_dist = dist
+                min_s = s
+        l = sigmoid(min_dist, theta, min_s.thresh)
+        loss_batch += l
+        n += 1
+    loss = loss * (n-w) / n +  loss_batch/n
+    loss_batch = loss_batch / w
+    # Check if there's a Concept Drift
+    if loss_batch > loss :
+        flag = True
+    else:
+        flag = False
     return 0
